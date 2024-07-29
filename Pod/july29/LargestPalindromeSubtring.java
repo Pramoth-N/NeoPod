9e586b3737cc932a7624e5040e2831c0546bca2a
Pramothsgp <jarvis.tech.006@mail.com>
2024-07-29 18:18:21 +0530

    July 29 Pod Iam Neo portal

---
+++ Pod/july29/LargestPalindromeSubtring.java
@@ -0,0 +1,59 @@
+/*
+ * 
+
+A palindrome string means that if we reverse the string, it gives you the same string.
+For example:
+         "madam" is a palindrome; if we reverse that string, it gives you the same word as "madam." 
+
+Now you have a string, and your task is to figure out the largest palindrome in it.
+
+Input format :
+The first line of input consists of a string, str.
+
+Output format :
+The output prints the largest palindrome of the given string.
+
+
+
+Refer to the sample input and output for formatting specifications.
+
+Code constraints :
+1 ≤ string.length ≤ 500
+
+ * 
+ */
+
+
+import java.util.*;
+
+class LargestPalindromeSubString {
+    public static void main(String args[]) {
+        String str = new String(new Scanner(System.in).nextLine());
+        String pal ="";
+        for(int i=0;i<str.length();i++) {
+            for(int j=str.length();j>i;j--){
+                String subStr = str.substring(i,j);
+                
+                if(isPal(subStr)) {
+                    if(pal.length() < subStr.length()){
+                        pal = subStr;
+                    }
+                }
+            }
+        }
+        
+        System.out.println(pal);
+    }
+    
+    private static boolean isPal(String str) {
+        int i=0,j=str.length() -1;
+        
+        while(i<j) {
+            if(str.charAt(i++) != str.charAt(j--)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
---
+++ Pod/july29/MaximumProfitInKdays.java
@@ -0,0 +1,64 @@
+/*
+ * 
+ * Imagine you are a sales manager at a retail store, and you need to analyze the daily sales data 
+ * to determine the maximum revenue you can achieve from any continuous period of K days.
+
+Example 1
+
+Input: 
+
+4 2
+
+100 200 300 400
+
+Output: 
+
+700
+
+Explanation:
+
+300 + 400 = 700, which is the maximum.
+
+Example 2
+
+Input:
+
+4 4
+
+100 200 300 400
+
+Output:
+
+1000
+
+Explanation:
+
+100 + 200 + 300 + 400 = 1000, which is maximum.
+ * 
+ */
+
+import java.util.*;
+
+class MaximumProfitInKdays {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int sales[] = new int[n];
+        
+        for(int i=0;i<n;i++) {
+            sales[i] = sc.nextInt();
+        }
+        
+        int maxProfit = 0;
+        
+        for(int i=0;i<=n-k;i++) {
+            int sum = 0;
+            for(int j=i;j<i+k;j++){
+                sum+=sales[j];
+            }
+            maxProfit = Math.max(maxProfit , sum);
+        }
+        System.out.println(maxProfit);
+    }
+}
