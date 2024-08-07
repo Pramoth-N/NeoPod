aced39b06777e272b2e92f802159319a97891656
Pramothsgp <jarvis.tech.006@mail.com>
2024-08-07 20:08:27 +0530

    August Pod

---
+++ Pod/august/august7/DoublePyramid.java
@@ -0,0 +1,79 @@
+/*
+ * 
+ * 
+ * Stephen wants to create a double pyramid pattern using asterisks based on a specified height. Write a program that reads the height of the pyramid and prints two mirrored pyramids side by side, with spaces between them. The pyramids should be aligned and centered correctly.
+
+
+
+Example
+
+
+
+Input: 
+
+7 // height
+
+
+
+Output:
+
+
+
+Input format :
+The input consists of an integer N, representing the height.
+
+Output format :
+The output displays the required pattern.
+
+
+
+Refer to the sample input and output for formatting specifications.
+
+Code constraints :
+2 ≤ N ≤ 20
+
+Sample test cases :
+Input 1 :
+7
+Output 1 :
+      *             * 
+     * *           * * 
+    * * *         * * * 
+   * * * *       * * * * 
+  * * * * *     * * * * * 
+ * * * * * *   * * * * * * 
+* * * * * * * * * * * * * * 
+Input 2 :
+4
+Output 2 :
+   *       * 
+  * *     * * 
+ * * *   * * * 
+* * * * * * * * 
+ * 
+ * 
+ */
+
+
+ import java.util.*;
+
+class DoublePyramid {
+    public static void main(String args[]){
+        int n = new Scanner(System.in).nextInt();
+        
+        
+        for(int i=1;i<=n;i++) {
+            if(i!=n)
+            System.out.printf("%"+(n-i)+"s","");
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            if(i!=n)
+            System.out.printf("%"+2*(n-i)+"s","");
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pod/august/august7/MixedArrangement.java
@@ -0,0 +1,129 @@
+/*
+ * 
+ * 
+ * Your company is organizing a team-building activity where you need to arrange employees into two distinct groups: one group for technical activities and the other for creative activities.
+
+
+
+The arrangement must ensure that:
+
+
+
+Technical employees (represented by positive integers) and creative employees (represented by negative integers) are alternated.
+The relative order within each group should be preserved.
+You have a list of employees where each employee is represented by a unique integer (positive for technical and negative for creative).
+
+
+Your task is to rearrange the list such that technical and creative employees are alternated while maintaining the order within each group.
+
+
+
+Write a program to rearrange the employees' list based on the given conditions.
+
+
+
+Example
+
+Input 
+
+6
+
+1 2 3 -4 -1 4
+
+Output 
+
+1 2 3 -4 -1 4  -------> Input list
+
+-4 1 -1 2 3 4  -------> Rearranged list
+
+
+
+Explanation
+
+Creative employees come first and are alternated with technical employees.
+
+The relative order within the technical and creative groups is preserved.
+
+
+
+Note: Here employee ID "0" represent technical employee.
+
+Input format :
+The first line consists of an integer N representing the number of employees.
+
+The second line consists of an array of integers of size N where each integer represents an employee's ID.
+
+Positive integers represent technical employees, and negative integers represent creative employees.
+
+Output format :
+The first line contains the elements of the original employee list before rearrangement.
+
+The second line contains the elements of the employee list after rearrangement according to the given conditions.
+
+
+
+Refer to the sample output for the formatting specifications.
+
+Code constraints :
+2 ≤ n ≤ 10
+
+-100 ≤ employees ID ≤100
+
+Sample test cases :
+Input 1 :
+6
+1 2 3 -4 -1 4
+Output 1 :
+1 2 3 -4 -1 4 
+-4 1 -1 2 3 4 
+Input 2 :
+10
+-5 -2 5 2 4 7 1 8 0 -8
+Output 2 :
+-5 -2 5 2 4 7 1 8 0 -8 
+-5 5 -2 2 -8 4 7 1 8 0 
+ * 
+ * 
+ */
+
+
+
+ import java.util.*;
+
+class MixedArrangement{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] emp = new int[n];
+        int neg = 0 , pos = 0;
+        for(int i=0;i<n;i++){
+            emp[i] = sc.nextInt();
+            if(emp[i] < 0)
+                neg++; 
+            else
+                pos++;
+        }
+        
+        int[] tech = new int[pos];
+        int[] creative = new int[neg];
+        int i=0 , j=0;
+        for(int k=0;k<n;k++){
+            System.out.print(emp[k] + " ");
+            if(emp[k]<0) 
+                creative[i++] = emp[k] ;
+            else
+                tech[j++] = emp[k];
+        }
+        System.out.print("\n");
+        i = j = 0;
+        while(i<neg && j<pos){
+            System.out.print(creative[i++] + " " + tech[j++] + " ");
+        }
+        while(i<neg){
+            System.out.print(creative[i++] + " " );
+        }
+        while(j<pos){
+            System.out.print(tech[j++] + " ");
+        }
+    }
+}
