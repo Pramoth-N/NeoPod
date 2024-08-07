dc25a61da9daa7603566bf5fa29356b0c4a90c03
Pramothsgp <jarvis.tech.006@mail.com>
2024-08-07 20:26:29 +0530

    Double pyramin patter

--- Pod/august/august7/DoublePyramid.java
+++ Pod/august/august7/DoublePyramid.java
@@ -1,31 +1,23 @@
 /*
  * 
  * 
- * Stephen wants to create a double pyramid pattern using asterisks based on a specified height. Write a program that reads the height of the pyramid and prints two mirrored pyramids side by side, with spaces between them. The pyramids should be aligned and centered correctly.
-
-
+ * Stephen wants to create a double pyramid pattern using asterisks based on a specified height. 
+ * Write a program that reads the height of the pyramid and prints two mirrored pyramids side by side,
+ *  with spaces between them. The pyramids should be aligned and centered correctly.
 
 Example
-
-
 
 Input: 
 
 7 // height
 
-
-
 Output:
-
-
 
 Input format :
 The input consists of an integer N, representing the height.
 
 Output format :
 The output displays the required pattern.
-
-
 
 Refer to the sample input and output for formatting specifications.
 
